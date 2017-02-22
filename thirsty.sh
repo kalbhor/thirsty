9b7e09279dcb261b995cfd7fab75db421df07fc1
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-02-22 20:38:27 +0530

    file handling

--- thirsty.sh
+++ thirsty.sh
@@ -1,13 +1,17 @@
-# WATER
-drink_water() {
-  if [[ THIRSTY == "false" ]]; then
-    last_time=$[$(date +%s) + 5]
-    echo -n "${THIRSTY}"
-    THIRSTY="true"
+# Water
 
-  elif [[ $[last_time] < $(date +%s) || THIRSTY!="false" ]]; then
-    THIRSTY="false"
-    echo -n "💧 You're thirsty ${THIRSTY}"
+drink_water() {
+thirsty=$(cat /Users/lakshay/.oh-my-zsh/custom/themes/config.txt)
+last_time=$(cat /Users/lakshay/.oh-my-zsh/custom/themes/config2.txt)
+
+  if [[ $thirsty == false ]]; then
+    echo "$[$(date +%s) + 10]" > /Users/lakshay/.oh-my-zsh/custom/themes/config2.txt
+    last_time=$(cat /Users/lakshay/.oh-my-zsh/custom/themes/config2.txt)
+    echo "true" > /Users/lakshay/.oh-my-zsh/custom/themes/config.txt
+    thirsty=$(cat /Users/lakshay/.oh-my-zsh/custom/themes/config.txt)
+
+  elif [[ $[last_time] -lt $(date +%s) ]]; then
+    echo -n "💧 You're thirsty"
   fi
 
 }
