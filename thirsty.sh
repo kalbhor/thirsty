70097d5696f7a73782ca6c2c02e1f11d4929a0e8
Lee Keitel <lfkeitel@usi.edu>
2017-02-23 14:27:13 -0600

    Better tests, another file reduction, added next_drink
    The tests actually check for proper output and not just display it.
    
    The true/false piece has been removed. Now the last drink time
    is saved so the next drink time can be calculated dynamically.
    
    Added next_drink function to display the time of the next drink.
    
    The time file is now configurable by setting the variable
    DRINK_WATER_CONF before calling in the script. This was mainly for
    testing, but can be useful.

--- thirsty.sh
+++ thirsty.sh
@@ -1,39 +1,27 @@
 #!/bin/sh
 
 WATER_TIME=${WATER_TIME:-10800} # Set time interval in seconds
-DRINK_WATER_CONF="$HOME/.water"
+DRINK_WATER_CONF="${DRINK_WATER_CONF:-$HOME/.water}"
 
 drink_water() {
   # If the file doesn't exist, create it
   if [ ! -f $DRINK_WATER_CONF ]; then
-    not_thirsty
+    date +%s > $DRINK_WATER_CONF
   fi
 
-  thirsty=$(head -1 $DRINK_WATER_CONF)
-  last_time=$(tail -1 $DRINK_WATER_CONF)
+  next_time=$(($(cat $DRINK_WATER_CONF) + $WATER_TIME))
 
-  if [ $thirsty = 'false' ]; then
-    echo "true" > $DRINK_WATER_CONF
-    echo "$(($(date +%s) + $WATER_TIME))" >> $DRINK_WATER_CONF
-    echo -n "Water is essential "
-  elif [ $last_time -lt $(date +%s) ]; then
+  if [ $next_time -lt $(date +%s) ]; then
     echo -n "💧 You're thirsty"
   fi
 }
 
 not_thirsty() {
-  echo "false" > $DRINK_WATER_CONF
-  echo "0" >> $DRINK_WATER_CONF
+  date +%s > $DRINK_WATER_CONF
+  echo "Water is essential"
 }
 
-# Username.
-# If user is root, then print it in red. Otherwise, just print in yellow.
-spaceship_user() {
-  if [ $USER = 'root' ]; then
-    echo -n "%{$fg_bold[red]%}"
-  else
-    echo -n "%{$fg_bold[yellow]%}"
-  fi
-  echo -n "%n"
-  echo -n "%{$reset_color%}"
+next_drink() {
+  next_time=$(($(cat $DRINK_WATER_CONF) + $WATER_TIME))
+  echo "Next drink at $(date --date="@$next_time")"
 }
