fb5eb5480eee6a4180a90759bad33a8fda7b1919
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-02-23 19:31:56 +0530

    Added essential message

--- thirsty.sh
+++ thirsty.sh
@@ -1,12 +1,13 @@
-water_time=20 # Set time interval in seconds
+water_time=10800 # Set time interval in seconds
 
 drink_water() {
-thirsty=`cat $HOME/.water` 
+thirsty=`cat $HOME/.water`
 last_time=`cat $HOME/.water_last_time`
 
   if [[ $thirsty == false ]]; then
     echo "$[$(date +%s) + $water_time]" > $HOME/.water_last_time
     echo "true" > $HOME/.water
+    echo -n " Water is essential "
 
   elif [[ $[last_time] -lt $(date +%s) ]]; then
     echo -n "💧 You're thirsty"
@@ -16,4 +17,15 @@ last_time=`cat $HOME/.water_last_time`
 not_thirsty() {
 echo "false" > $HOME/.water
 echo "0" > $HOME/.water_last_time
+}
+# Username.
+# If user is root, then pain it in red. Otherwise, just print in yellow.
+spaceship_user() {
+  if [[ $USER == 'root' ]]; then
+    echo -n "%{$fg_bold[red]%}"
+  else
+    echo -n "%{$fg_bold[yellow]%}"
+  fi
+  echo -n "%n"
+  echo -n "%{$reset_color%}"
 }
