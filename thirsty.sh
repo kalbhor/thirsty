74bd342ceb1388f944752b4504cfb1e4271009c0
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-02-23 16:48:56 +0530

    Removed file handling

--- thirsty.sh
+++ thirsty.sh
@@ -1,14 +1,14 @@
-# Water
+thirsty=
+last_time=
 
 drink_water() {
-thirsty=$(cat /Users/lakshay/.oh-my-zsh/custom/themes/config.txt)
-last_time=$(cat /Users/lakshay/.oh-my-zsh/custom/themes/config2.txt)
+echo -n "$thirsty"
+water_time=10 # Set time interval 
 
   if [[ $thirsty == false ]]; then
-    echo "$[$(date +%s) + 10]" > /Users/lakshay/.oh-my-zsh/custom/themes/config2.txt
-    last_time=$(cat /Users/lakshay/.oh-my-zsh/custom/themes/config2.txt)
-    echo "true" > /Users/lakshay/.oh-my-zsh/custom/themes/config.txt
-    thirsty=$(cat /Users/lakshay/.oh-my-zsh/custom/themes/config.txt)
+    last_time="$[$(date +%s) + $water_time]"
+    thirsty="true"
+    echo -n "${last_time} $(date +%s) ${thirsty}"
 
   elif [[ $[last_time] -lt $(date +%s) ]]; then
     echo -n "💧 You're thirsty"
